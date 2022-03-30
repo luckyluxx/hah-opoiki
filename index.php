c9f865a590b05e981b84639c291853dc53028890
luckyluxx <luckyluck5283@gmail.com>
2022-03-30 13:11:18 +0700

    prila

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+	
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	
+ 	<!-- favicon -->
+ 	 <link rel="icon" type="image/x-icon" href="favicon.png">
+
+ 	<!-- bootstrap style -->
+ 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ 	<!-- custom -->
+ 	<link rel="stylesheet" type="text/css" href="custom.css">
+ 	
+ 	<title>Happy Birthday a Pearl!</title>
+ </head>
+ <body>
+ 	<div class="warnabg">
+ 	<div class="container mt-5">
+ 		<div class="row align-items-center">
+ 			<div class="col align-self-center">
+ 				<div class="card position-absolute top-50 start-50 translate-middle shadow-lg p-3 mb-5 bg-body rounded" style="width: 18rem;">
+ 					<a href="http://multimediagrafika.com/ta2020/mma/08.apk" target="_blank">
+ 						<img src="apkaprel.png" class="card-img-top" alt="...">
+ 					</a>
+ 					<div class="card-body align-items-center">
+ 						<p class="card-text ">Happy Birthday <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSHxwTLtHqFXWGzhgSNndTgrkPhNDbprSglXMqsjQFzNDgKKgghZXNMdmSzhKvqXwgFMFLbd" class="link-info" target="_blank"> @apermatas01@gmail.com </a></p>
+ 					</div>
+ 				</div>
+ 			</div>	
+ 		</div>
+ 	</div>
+ 	</div>
+ 
+ </body>
+ </html>
